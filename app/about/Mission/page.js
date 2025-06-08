62e9be2e86467d6c9ea38fdc68f28cf3ca4ad348
khadiza40 <bibikhadiza277@gmail.com>
2025-06-08 17:34:56 +0600

    server vs client components

--- app/about/Mission/page.js
+++ app/about/Mission/page.js
@@ -1,7 +1,21 @@
+import Button from '@/app/components/Button'
 import React from 'react'
 
 export default function Mission() {
   return (
-    <div>This is mission  page. </div>
+     <main className="mt-10">
+            <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Excepturi fugit itaque delectus ipsum id amet voluptatem hic
+                repellat, error corporis asperiores est perspiciatis tempora
+                consequatur cupiditate soluta aperiam eos voluptates.
+            </div>
+
+            {/* <div className="w-[400px]">
+                <Image placeholder="blur" src={thumb} alt="Thumb image" />
+            </div> */}
+
+            <Button />
+        </main>
   )
 }
---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+
+    return (
+        <main className="mt-10">
+            <div>About us page</div>
+        </main>
+    );
+}
